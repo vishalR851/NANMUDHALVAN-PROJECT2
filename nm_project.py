4d18c62cb545b393526d9917d791fb4362fc6e35
vishalR851 <vishalramajeyam@gmail.com>
2025-04-25 10:16:38 +0530

    Update nm_project.py

--- nm_project.py
+++ nm_project.py
@@ -1,17 +1,4 @@
-# Imports
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
-import shap
-import matplotlib.pyplot as plt
-import seaborn as sns 
+
 
 # Load dataset
 df = pd.read_csv('/content/drive/MyDrive/DATASETS/churn.csv') 
